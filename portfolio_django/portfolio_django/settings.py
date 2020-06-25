c1f733f196f9ad7e7f4d6ec9d1e137282bbdb61f
Andy Riecker <ariecker@gmail.com>
2020-06-25 11:03:47 -0400

    getting ready to host

--- portfolio_django/portfolio_django/settings.py
+++ portfolio_django/portfolio_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,13 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f)54yb-d$bldsv!_lxh2)_)*$zfpe%yg$5&_6uk@crh5u8*uq9'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['178.128.147.169', '127.0.0.1', 'localhost', 'andrewriecker.com']
 
 
 # Application definition
